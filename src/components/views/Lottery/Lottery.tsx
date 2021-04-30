f11104d93f8397843379d134d6c36beae267c91e
DESKTOP-PNQ3FLQ\jahhy <jah.hyun12345@gmail.com>
2021-04-30 21:09:38 +0900

    * class component => hooks component * useEffect : timeouts.current 값 바뀔 때마다 udpate => willunmount return 값도 설정 * useState 값에 type 설정하여 오류 방지

--- src/components/views/Lottery/Lottery.tsx
+++ src/components/views/Lottery/Lottery.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Ball from './Sections/Ball';
 
+// state 사용하지 않는 method 편리 위해 분리
 function getWinNumbers() {
     console.log('getWinNumbers called');
     const candidate = [];
@@ -16,66 +17,134 @@ function getWinNumbers() {
     return [...winNumbers, bonusNumber];
 }
 
-interface lotteryProps {
-}
+export default function Lottery() {
+    type WinNumbers = any;
+    const [winNumbers, setWinNumbers] = useState<WinNumbers>(getWinNumbers());
+    const [winBalls, setWinBalls] = useState(Array());
+    const [bonus, setBonus] = useState(null);
+    const [redo, setRedo] = useState(false);
+    const timeouts:any = React.useRef([]);
 
-interface lotteryState {
-    winNumbers:any,
-    winBalls:any,
-    bonus:any,
-    redo:any
-}
+    useEffect(() => {
+        runTimeouts();
+        return () => {
+            timeouts.current.forEach((v:any) => {
+                clearTimeout(v);
+            })
+        }
+    }, [timeouts.current])
 
-export default class Lottery extends React.Component<lotteryProps, lotteryState> {
-    state:lotteryState = {
-        winNumbers:getWinNumbers(),
-        winBalls:[],
-        bonus:null,
-        redo:false
-    }
-
-    timeouts:any = [];
-
-    componentDidMount() {
-        const {winNumbers} = this.state;
-        for (let i = 0; i < (this.state.winNumbers.length - 1); i++) {
-            this.timeouts[i] = setTimeout(() => {
-                this.setState((prevState) => {
-                    return {
-                        winBalls:[...prevState.winBalls, winNumbers[i]]
-                    }
-                })
+    const runTimeouts = () => {
+        for (let i = 0; i < (winNumbers.length - 1); i++) {
+            timeouts.current[i] = setTimeout(() => {
+                setWinBalls((prevWinBalls:any) => [...prevWinBalls, winNumbers[i]]);
             // 1st : 1s / 2nd : 2s / 3rd : 3s...
             }, (i + 1) * 1000);
         }
-        this.timeouts[6] = setTimeout(() => {
-            this.setState({bonus:winNumbers[6], redo:true});
+        timeouts.current[6] = setTimeout(() => {
+            setBonus(winNumbers[6]);
+            setRedo(true);
         }, 7000);
     }
 
-    componentWillUnmount() {
-        // timeouts 배열 값 전부 지울 수 있도록
-        this.timeouts.forEach((v:any) => {
-            clearTimeout(v);
-        })
+    const onClickRedo = () => {
+        setWinNumbers(getWinNumbers());
+        setWinBalls([]);
+        setBonus(null);
+        setRedo(false);
+        timeouts.current = [];
     }
 
-    onClickRedo = () => {
-        console.log('click')
-    }
-
-    render() {
-        const {winNumbers, winBalls, bonus, redo} = this.state;
-        return(
-            <React.Fragment>
-                <div>Win Number</div>
-                <div id="result">
-                    {winBalls.map((v:any) => <Ball key={v} number={v} />)}
-                </div>
-                <div>Bonus Number</div>
-                {bonus && <Ball number={bonus} />}
-                {redo && <button onClick={this.onClickRedo}>One More!</button>}
-            </React.Fragment>
-        );
-    }
+    return(
+        <React.Fragment>
+            <div>Win Number</div>
+            <div id="result">
+                {winBalls.map((v:any) => <Ball key={v} number={v} />)}
+            </div>
+            <div>Bonus Number</div>
+            {bonus && <Ball number={bonus} />}
+            {redo && <button onClick={onClickRedo}>One More!</button>}
+        </React.Fragment>
+    );
 }
+
+// interface lotteryProps {
+// }
+
+// interface lotteryState {
+//     winNumbers:any,
+//     winBalls:any,
+//     bonus:any,
+//     redo:any
+// }
+
+// export default class Lottery extends React.Component<lotteryProps, lotteryState> {
+//     state:lotteryState = {
+//         winNumbers:getWinNumbers(),
+//         winBalls:[],
+//         bonus:null,
+//         redo:false
+//     }
+
+//     timeouts:any = [];
+
+//     runTimeouts = () => {
+//         const {winNumbers} = this.state;
+//         for (let i = 0; i < (this.state.winNumbers.length - 1); i++) {
+//             this.timeouts[i] = setTimeout(() => {
+//                 this.setState((prevState) => {
+//                     return {
+//                         winBalls:[...prevState.winBalls, winNumbers[i]]
+//                     }
+//                 })
+//             // 1st : 1s / 2nd : 2s / 3rd : 3s...
+//             }, (i + 1) * 1000);
+//         }
+//         this.timeouts[6] = setTimeout(() => {
+//             this.setState({bonus:winNumbers[6], redo:true});
+//         }, 7000);
+//     }
+
+//     componentDidMount() {
+//         this.runTimeouts();
+//     }
+
+//     componentDidUpdate(prevProps:any, prevState:any) {
+//         // if condition 설정 필요
+//         if (this.state.winBalls.length === 0) {
+//             this.runTimeouts();
+//         }
+//     }
+
+//     componentWillUnmount() {
+//         // timeouts 배열 값 전부 지울 수 있도록
+//         this.timeouts.forEach((v:any) => {
+//             clearTimeout(v);
+//         })
+//     }
+
+//     onClickRedo = () => {
+//         this.setState({
+//             winNumbers:getWinNumbers(),
+//             winBalls:[],
+//             bonus:null,
+//             redo:false
+//         })
+//         this.timeouts = [];
+//     }
+
+//     render() {
+//         const {winNumbers, winBalls, bonus, redo} = this.state;
+//         return(
+//             <React.Fragment>
+//                 <div>Win Number</div>
+//                 <div id="result">
+//                     {winBalls.map((v:any) => <Ball key={v} number={v} />)}
+//                 </div>
+//                 <div>Bonus Number</div>
+//                 {bonus && <Ball number={bonus} />}
+//                 {redo && <button onClick={this.onClickRedo}>One More!</button>}
+//             </React.Fragment>
+//         );
+//     }
+// }
