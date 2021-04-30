07d5506761a69e4b86d886e464ed9e6e8e459cac
DESKTOP-PNQ3FLQ\jahhy <jah.hyun12345@gmail.com>
2021-04-30 09:44:41 +0900

    * timeout / startTime / endTime variable in function component => funciton component에서 this.xxx로 호출 잘 사용하지 않음 => state 값으로 정의할 경우 불필요한 랜더링 발생 가능성 있음 => useRef() 값으로 정의한 뒤 xxx.current 값을 설정하여 사용 => useRef() : 랜더링 등 재 실행 X

--- src/components/views/Responsecheck/Responsecheck.tsx
+++ src/components/views/Responsecheck/Responsecheck.tsx
@@ -1,138 +1,150 @@
 import React, {useState} from 'react';
 import './Responsecheck.css';
 
-interface ResponsecheckProps {
-}
+export default function Responsecheck() {
+    const [state, setState] = useState('waiting');
+    const [message, setMessage] = useState('Click to Start!');
+    const [result, setResult] = useState(Array());
+    const timeout:any = React.useRef(null);
+    const startTime:any = React.useRef();
+    const endTime:any = React.useRef();
 
-interface ResponsecheckState {
-    state:any,
-    message:any,
-    result:any
-}
-
-export default class Responsecheck extends React.Component<ResponsecheckProps, ResponsecheckState> {
-    state:ResponsecheckState = {
-        state:'waiting',
-        message:'Click to Start!',
-        result:[]
-    }
-
-    timeout:any;
-    startTime:any;
-    endTime:any;
-
-    onClickScreen = () => {
-        const { state, message, result} = this.state;
+    const onClickScreen = () => {
         if (state === 'waiting') {
-            this.setState({
-                state:'ready',
-                message:'Click when it\'s Green!'
-            })
-            this.timeout = window.setTimeout(() => {
-                this.setState({
-                    state:'now',
-                    message:'Click Now'
-                })
-                this.startTime = new Date();
+            setState('ready');
+            setMessage('Click when it\'s Green!');
+            timeout.current = window.setTimeout(() => {
+                setState('now');
+                setMessage('Click Now!');
+                startTime.current = new Date();
                 // 2~3초 랜덤
             }, (Math.floor(Math.random() * 1000) + 2000));
-            console.log({timeout_1:this.timeout});
+            console.log({timeout_1:timeout.current});
         } else if (state === 'ready') {
-            console.log({timeout_2:this.timeout});
-            window.clearTimeout(this.timeout);
-            this.setState({
-                state:'waiting',
-                message:'Too fast! Click when it\'s Green!'
-            })
+            console.log({timeout_2:timeout.current})
+            window.clearTimeout(timeout.current);
+            setState('waiting');
+            setMessage('Too fast! Click when it\'s Green!');
         } else if (state === 'now') {
-            this.endTime = new Date();
-            this.setState((prevState) => {
-                return {
-                    state:'waiting',
-                    message:'Click to Start!',
-                    result:[...prevState.result, (this.endTime - this.startTime)]
-                }
-            })
+            endTime.current = new Date();
+            setState('waiting');
+            setMessage('Click to Start!');
+            setResult((prevResult:any) => {
+                return [...prevResult, (endTime.current - startTime.current)]
+            });
         }
     }
 
-    onReset = () => {
-        this.setState({result:[]});
+    const onReset = () => {
+        setResult([]);
     }
 
-    renderAverage = () => {
-        const { result } = this.state;
+    const renderAverage = () => {
         return (
             result.length > 0 &&
             <React.Fragment>
-                <div>Average-Time : {result.reduce((a:any, c:any) => a + c) / result.length}ms</div>
-                <button onClick={this.onReset}>reset</button>
+                <div>Average Time : {result.reduce((a:any, c:any) => a + c) / result.length}ms</div>
+                <button onClick={onReset}>reset</button>
             </React.Fragment>
         );
     }
-    
-    render() {
-        return (
+
+    return (
         <React.Fragment>
-            <div
-                id="screen"
-                className={this.state.state}
-                onClick={this.onClickScreen}
-            >
-                {this.state.message}
-            </div>
-            {this.renderAverage()}
+        <div
+            id="screen"
+            className={state}
+            onClick={onClickScreen}
+        >
+            {message}
+        </div>
+        {renderAverage()}
         </React.Fragment>
-        );
-    }
+    );
 }
 
-// export default function Responsecheck() {
-//     const [state, setState] = useState('waiting');
-//     const [message, setMessage] = useState('Click to Start!');
-//     const [result, setResult] = useState(Array());
+// interface ResponsecheckProps {
+// }
 
-//     let timeout = 0;
+// interface ResponsecheckState {
+//     state:any,
+//     message:any,
+//     result:any
+// }
 
-//     const onClickScreen = () => {
+// export default class Responsecheck extends React.Component<ResponsecheckProps, ResponsecheckState> {
+//     state:ResponsecheckState = {
+//         state:'waiting',
+//         message:'Click to Start!',
+//         result:[]
+//     }
+
+//     timeout:any;
+//     startTime:any;
+//     endTime:any;
+
+//     onClickScreen = () => {
+//         const { state, message, result} = this.state;
 //         if (state === 'waiting') {
-//             setState('ready');
-//             setMessage('Click when it\'s Green!');
-//             timeout = window.setTimeout(() => {
-//                 setState('now');
-//                 setMessage('Click Now!');
+//             this.setState({
+//                 state:'ready',
+//                 message:'Click when it\'s Green!'
+//             })
+//             this.timeout = window.setTimeout(() => {
+//                 this.setState({
+//                     state:'now',
+//                     message:'Click Now'
+//                 })
+//                 this.startTime = new Date();
 //                 // 2~3초 랜덤
 //             }, (Math.floor(Math.random() * 1000) + 2000));
-//             console.log({timeout_1:timeout});
+//             console.log({timeout_1:this.timeout});
 //         } else if (state === 'ready') {
-//             console.log({timeout_2:timeout})
-//             // window.clearTimeout(timeout);
-//             setState('waiting');
-//             setMessage('Too fast! Click when it\'s Green!');
+//             console.log({timeout_2:this.timeout});
+//             window.clearTimeout(this.timeout);
+//             this.setState({
+//                 state:'waiting',
+//                 message:'Too fast! Click when it\'s Green!'
+//             })
 //         } else if (state === 'now') {
-//             setState('waiting');
-//             setMessage('Click to Start!');
-//             setResult([]);
+//             this.endTime = new Date();
+//             this.setState((prevState) => {
+//                 return {
+//                     state:'waiting',
+//                     message:'Click to Start!',
+//                     result:[...prevState.result, (this.endTime - this.startTime)]
+//                 }
+//             })
 //         }
 //     }
 
-//     const renderAverage = () => {
-//         return (
-//             result.length > 0 &&
-//             <div>Average Time : {result.reduce((a:any, c:any) => a + c) / result.length}ms</div>
-//         );
+//     onReset = () => {
+//         this.setState({result:[]});
 //     }
 
-//     return (
+//     renderAverage = () => {
+//         const { result } = this.state;
+//         return (
+//             result.length > 0 &&
+//             <React.Fragment>
+//                 <div>Average-Time : {result.reduce((a:any, c:any) => a + c) / result.length}ms</div>
+//                 <button onClick={this.onReset}>reset</button>
+//             </React.Fragment>
+//         );
+//     }
+    
+//     render() {
+//         return (
 //         <React.Fragment>
-//         <div
-//             id="screen"
-//             className={state}
-//             onClick={onClickScreen}
-//         >
-//             {message}
-//         </div>
-//         {renderAverage()}
+//             <div
+//                 id="screen"
+//                 className={this.state.state}
+//                 onClick={this.onClickScreen}
+//             >
+//                 {this.state.message}
+//             </div>
+//             {this.renderAverage()}
 //         </React.Fragment>
-//     );
+//         );
+//     }
 // }
