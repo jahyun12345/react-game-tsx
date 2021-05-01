7d07d56fba24471426dfd7e3388f10eddc7e7d61
DESKTOP-PNQ3FLQ\jahhy <jah.hyun12345@gmail.com>
2021-05-01 18:13:27 +0900

    * useMemo get error => memo 사용하도록 수정

--- src/components/views/Tiktaktoe/Sections/Tr.tsx
+++ src/components/views/Tiktaktoe/Sections/Tr.tsx
@@ -1,22 +1,23 @@
-import React, {useMemo} from 'react';
+import React, {useMemo, memo} from 'react';
 import Td from './Td';
 
-const Tr = (props:any) =>{
+const Tr = memo((props:any) =>{
     // rowData:['','','']
     const {rowData, rowIndex, dispatch} = props;
     
     return(
         <tr>
             {rowData.map((td:any, index:any) => (
-                // useMemo : component 자체 기억
-                // rowData[i] 값이 바뀌었을 때만 랜더링 되도록 설정
-                useMemo(() =>
-                    <Td key={index} rowIndex={rowIndex} cellIndex={index} cellData={rowData[index]} dispatch={dispatch} />,
-                    [rowData[index]]
-                )
+                <Td key={index} rowIndex={rowIndex} cellIndex={index} cellData={rowData[index]} dispatch={dispatch} />
+                // // useMemo : component 자체 기억
+                // // rowData[i] 값이 바뀌었을 때만 랜더링 되도록 설정
+                // useMemo(() =>
+                //     <Td key={index} rowIndex={rowIndex} cellIndex={index} cellData={rowData[index]} dispatch={dispatch} />,
+                //     [rowData[index]]
+                // )
             ))}
         </tr>
     );
-}
+})
 
 export default Tr;
